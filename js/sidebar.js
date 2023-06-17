ed19a7b0386debe1bdb3942fc3d193432bbb9bd3
Dmytro Asparian <supezbiz@gmail.com>
2023-06-17 13:15:19 +0300

    Added burger-menu animation

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,10 +2,12 @@ const burger = document.querySelector('.burger');
 const sidebar = document.querySelector('.sidebar');
 const closeButton = document.querySelector('.sidebar__close');
 
-burger.addEventListener('click', () => {
+burger.addEventListener('click', function() {
   sidebar.classList.toggle('sidebar--open');
+  burger.classList.toggle('change'); 
 });
 
 closeButton.addEventListener('click', () => {
   sidebar.classList.remove('sidebar--open');
+  burger.classList.remove('change'); 
 });
